90c86f32735d2e89da0ddeb10becd2e5be717ef2
jws2017 <34115792+jws2017@users.noreply.github.com>
2021-05-18 21:39:01 -0500

    repaired clustering section began work on tree
    TODO:
    plot other clusters
    complete tree analysis
    write analysis

--- crx.r
+++ crx.r
@@ -5,6 +5,7 @@
 #---
 
 ####Retrieval and Initial Exploration of the Data####
+
 crx <- read.csv("https://archive.ics.uci.edu/ml/machine-learning-databases/credit-screening/crx.data", stringsAsFactors = TRUE, na.strings = "?")
 show(crx)
 str(crx)
@@ -23,10 +24,18 @@ hist(crx$X1.25)
 crx$X. <- as.logical(as.numeric(crx$X.)-1)
 
 ####Clustering####
-clust <- hclust(dist(crx))
-kclust4 <- kmeans(crx, 4)
-kclust6 <- kmeans(crx, 6)
-kclust8 <- kmeans(crx, 8)
+
+crx.copy <- crx
+for (column in names(crx.copy)) {
+  if (class(crx.copy[,column]) == "factor") crx.copy[,column] <- as.integer(crx.copy[,column])
+}
+clust <- hclust(dist(crx.copy))
+kclust4 <- kmeans(crx.copy, 4)
+kclust6 <- kmeans(crx.copy, 6)
+kclust8 <- kmeans(crx.copy, 8)
+
+plot(clust)
+
 ####Association Rules#####
 library("arules")
 library("arulesViz")
@@ -35,4 +44,13 @@ crxRules <- apriori(crxT, parameter = list(confidence = 0.8, support = 0.25, min
 print(inspect(crxRules))
 
 ####Decision Trees####
-# TODO: Perform decision-tree analysis. Split the data into test and training data, use the training data to generate trees, evaluate using test data.
+
+trainingData <- crx[caTools::sample.split(crx$X.),]
+testData <- crx[-as.integer(row.names(trainingData)),]
+tree <- rpart::rpart(X. ~ ., trainingData)
+rpart.plot::rpart.plot(tree)
+prediction <- predict(tree, testData)
+pprob <- predict(tree, testData, type = "prob")
+confusionMatrix <- table(predicted = prediction, actual = testData$X.)
+ROC <- pROC::roc(testData$X., pprob)
+plot(ROC)

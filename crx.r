dbba8971ea802b2baabd5f5b2483cfdacd017e45
jws2017 <34115792+jws2017@users.noreply.github.com>
2021-05-18 23:46:57 -0500

    changed input to exclude headers cleaned up histogram plotting noticed that numeric data was exponential, so used log added boxplots removed erroneous data cleaning added task cleaned up crx.copy cleaned tree section

--- crx.r
+++ crx.r
@@ -6,28 +6,29 @@
 
 ####Retrieval and Initial Exploration of the Data####
 
-crx <- read.csv("https://archive.ics.uci.edu/ml/machine-learning-databases/credit-screening/crx.data", stringsAsFactors = TRUE, na.strings = "?")
+crx <- read.csv("https://archive.ics.uci.edu/ml/machine-learning-databases/credit-screening/crx.data", header = FALSE, stringsAsFactors = TRUE, na.strings = "?")
 show(crx)
 str(crx)
 
 ####Data Cleaning and Preparation#####
+
 crx <- crx[complete.cases(crx),]
 for (i in 1:length(crx)) {
-  if (class(crx[,i]) == "numeric" | class(crx[,i]) == "integer") hist(crx[,i], xlab = names(crx)[i], main = paste("Histogram of", names(crx)[i]))
+  if (is.numeric(crx[,i])) {
+    hist(crx[,i])
+    crx[,i] <- log(crx[,i] + 1)
+    hist(crx[,i], main = paste("Histogram (after) of", names(crx)[i]))
+    boxplot(crx[,i], main = paste("Boxplot for", names(crx)[i]))
+  }
 }
-crx <- crx[crx$X01 < 67,]
-hist(crx$X01)
-crx <- crx[crx$X00202 < 2000,]
-hist(crx$X00202)
-crx <- crx[crx$X1.25 < 20,]
-hist(crx$X1.25)
-crx$X. <- as.logical(as.numeric(crx$X.)-1)
+
+#TODO: Decide what to do about the outliers
 
 ####Clustering####
 
 crx.copy <- crx
 for (column in names(crx.copy)) {
-  if (class(crx.copy[,column]) == "factor") crx.copy[,column] <- as.integer(crx.copy[,column])
+  if (is.factor(crx.copy[,column])) crx.copy[,column] <- as.integer(crx.copy[,column])
 }
 clust <- hclust(dist(crx.copy))
 kclust4 <- kmeans(crx.copy, 4)
@@ -45,12 +46,12 @@ print(inspect(crxRules))
 
 ####Decision Trees####
 
-trainingData <- crx[caTools::sample.split(crx$X.),]
+trainingData <- crx[caTools::sample.split(crx[,16]),]
 testData <- crx[-as.integer(row.names(trainingData)),]
 tree <- rpart::rpart(X. ~ ., trainingData)
 rpart.plot::rpart.plot(tree)
 prediction <- predict(tree, testData)
 pprob <- predict(tree, testData, type = "prob")
-confusionMatrix <- table(predicted = prediction, actual = testData$X.)
-ROC <- pROC::roc(testData$X., pprob)
+confusionMatrix <- table(predicted = prediction, actual = testData[,16])
+ROC <- pROC::roc(testData[,16], pprob)
 plot(ROC)
